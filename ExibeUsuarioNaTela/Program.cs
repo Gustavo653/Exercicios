d2bdc71190650bf11a9ddd7640d1104f008d6f6e
Gustavo Henrique da Silva <gustavohs2004@gmail.com>
2021-03-09 14:01:45 -0300

    Projeto ExibeUsuarioNaTela feito

---
+++ ExibeUsuarioNaTela/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExibeUsuarioNaTela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira seu nome de usuário: ");
+            string usuario = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Seu nome de usuário é: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(usuario);
+        }
+    }
+}
