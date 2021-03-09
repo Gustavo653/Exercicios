7cb6e8a47850dbe8636a684fb67ab68b696a1282
Gustavo Henrique da Silva <gustavohs2004@gmail.com>
2021-03-09 20:48:21 -0300

    Projeto que verifica um tipo de triangulo adicionado

---
+++ Triangulos/Entities/VerificarTriangulo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Triangulos.Entities
+{
+    class VerificarTriangulo
+    {
+        public int N1 { get; set; }
+        public int N2 { get; set; }
+        public int N3 { get; set; }
+
+        public VerificarTriangulo(int n1, int n2, int n3)
+        {
+            N1 = n1;
+            N2 = n2;
+            N3 = n3;
+        }
+
+        public string Verificacao(int n1, int n2, int n3)
+        {
+            int sum1, sum2, sum3;
+            sum1 = n2 + n3;
+            sum2 = n1 + n3;
+            sum3 = n1 + n2;
+            if (n1 <= sum1 && n2 <= sum2 && n3 <= sum3)
+            {
+                if (n1 == n2 && n1 == n3)
+                {
+                    return "Triângulo Equilátero";
+                }
+
+                if (n1 == n2 && n1 != n3)
+                {
+                    return "Triângulo Isósceles";
+                }
+                else if (n1 == n3 && n1 != n3)
+                {
+                    return "Triângulo Isósceles";
+                }
+
+                if (n1 != n2 && n1 != n3 && n2 != n3)
+                {
+                    return "Triângulo Escaleno";
+                }
+            }
+            return "Não forma triângulo";
+        }
+    }
+}
---
+++ Triangulos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Triangulos.Entities;
+
+namespace Triangulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Informe o primeiro valor: ");
+                int n1 = int.Parse(Console.ReadLine());
+                Console.Write("Informe o segundo valor: ");
+                int n2 = int.Parse(Console.ReadLine());
+                Console.Write("Informe o terceiro valor: ");
+                int n3 = int.Parse(Console.ReadLine());
+                VerificarTriangulo veftri = new VerificarTriangulo(n1, n2, n3);
+                Console.WriteLine(veftri.Verificacao(n1, n2, n3));
+                Main(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Erro inesperado!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
