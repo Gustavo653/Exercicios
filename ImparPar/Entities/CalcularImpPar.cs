12d90515e3efc847e57ab5f46f699ba1576a85cb
Gustavo Henrique da Silva <gustavohs2004@gmail.com>
2021-03-09 13:57:00 -0300

    Projeto criado, exercicio que detecta pares e impares feito.

---
+++ ImparPar/Entities/CalcularImpPar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImparPar.Entities
+{
+    class CalcularImpPar
+    {
+        public int N1 { get; set; }
+
+        public CalcularImpPar(int n1)
+        {
+            N1 = n1;
+        }
+
+        public string Verificar (int n1)
+        {
+            if (n1 % 2 == 0)
+            {
+                return "O valor é par.";
+            }
+            else
+            {
+                return "O valor é impar.";
+            }
+        }
+    }
+}
---
+++ ImparPar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ImparPar.Entities;
+
+namespace ImparPar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número");
+            int n = int.Parse(Console.ReadLine());
+            CalcularImpPar calc = new CalcularImpPar(n);
+            Console.WriteLine(calc.Verificar(n));
+        }
+    }
+}
