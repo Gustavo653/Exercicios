c14c33e44bc60ee5ef473338a8b19395c80988eb
Gustavo Henrique da Silva <gustavohs2004@gmail.com>
2021-03-09 19:28:37 -0300

    Projeto calculadora concluido

---
+++ Exercicios/Entities/Calculos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora.Entities
+{
+    class Calculos
+    {
+        public string Op { get; set; }
+        public int N1 { get; set; }
+        public int N2 { get; set; }
+
+        public Calculos(string op, int n1, int n2)
+        {
+            Op = op;
+            N1 = n1;
+            N2 = n2;
+        }
+
+        public int Calcular (string op, int n1, int n2)
+        {
+            if (op == "+")
+            {
+                int soma = n1 + n2;
+                return soma;
+            }
+            else if (op == "-")
+            {
+                int sub = n1 - n2;
+                return sub;
+            }
+            else if (op == "*")
+            {
+                int mult = n1 * n2;
+                return mult;
+            }
+            else
+            {
+                int div = n1 / n2;
+                return div;
+            }
+        }
+    }
+}
--- Exercicios/Program.cs
+++ Exercicios/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using Calculadora.Entities;
 
-namespace Exercicios
+namespace Calculadora
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.Write("Insira a operação: ( + | - | * | / )");
+                string op = Console.ReadLine();
+                Console.Write("Insira o primeiro valor: ");
+                int n1 = int.Parse(Console.ReadLine());
+                Console.Write("Insira o primeiro valor: ");
+                int n2 = int.Parse(Console.ReadLine());
+                Calculos calc = new Calculos(op, n1, n2);
+                Console.WriteLine(calc.Calcular(op, n1, n2));
+                Main(args);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Impossível dividir por zero!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Formato inválido!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Erro desconhecido!");
+            }
         }
     }
 }
