0031db9d02a9d64bd6139238390ea49b4fd06a42
Gustavo Henrique da Silva <gustavohs2004@gmail.com>
2021-03-11 13:33:01 -0300

    Projeto Ano Bissexto

---
+++ AnoBissexto/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AnoBissexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Anos bissextos entre 1900 e 2030.\n");
+            int contador = 0;
+            for (int ano = 1900; ano <= 2030; ano++)
+            {
+                if (DateTime.IsLeapYear(ano)) //Metodo que verifica o ano
+                {
+                    contador++;
+                    Console.Write($"\t{ano}");
+                }
+            }
+            Console.WriteLine($"\nTotal de {contador} anos bissextos entre 1900 e 2030.\n");
+            Console.ReadLine();
+        }
+    }
+}
