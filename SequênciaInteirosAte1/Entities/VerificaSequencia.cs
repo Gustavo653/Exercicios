a4582eca54b4fbdd60b2a9153c9739471fff5818
Gustavo Henrique da Silva <gustavohs2004@gmail.com>
2021-03-11 13:50:08 -0300

    Projeto SequenciaInteiros Criado

---
+++ SequênciaInteirosAte1/Entities/VerificaSequencia.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SequenciaInteirosAte1.Entities
+{
+    class VerificaSequencia
+    {
+        public int Numero { get; set; }
+
+        public VerificaSequencia(int numero)
+        {
+            Numero = numero;
+        }
+
+        public int Sequencia(int n)
+        {
+            int cont = 0;
+            Console.WriteLine(n);
+            while (n != 1)
+            {
+                if (n % 2 == 0)
+                {
+                    n /= 2;
+                    Console.WriteLine(n);
+                }
+                else
+                {
+                    n *= 3;
+                    n += 1;
+                    Console.WriteLine(n);
+                }
+                cont++;
+            }
+            Console.Write("A quantidade de operações foi de: ");
+            return cont;
+        }
+    }
+}
---
+++ SequênciaInteirosAte1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SequenciaInteirosAte1.Entities;
+
+namespace SequenciaInteirosAte1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Insira um número: ");
+                int n = int.Parse(Console.ReadLine());
+                VerificaSequencia sequencia = new VerificaSequencia(n);
+                Console.WriteLine(sequencia.Sequencia(n));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro inesperado!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
