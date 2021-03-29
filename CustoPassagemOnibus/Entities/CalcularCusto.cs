de7766ce349c2cbf54bf5bd2be2aa5d92c549894
Gustavo653 <gustavohs2004@gmail.com>
2021-03-29 16:39:54 -0300

    CustoPassagemOnibus criado

---
+++ CustoPassagemOnibus/Entities/CalcularCusto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustoPassagemOnibus.Entities
+{
+    class CalcularCusto
+    {
+        public double Kilometer { get; set; }
+        public char Choice { get; set; }
+
+        public CalcularCusto(double kilometer, char choice)
+        {
+            Kilometer = kilometer;
+            Choice = choice;
+        }
+
+        public double Calcular(double kilometer, char choice)
+        {
+            double fare = 9.0;
+            if (kilometer == 0)
+            {
+                Environment.Exit(0);
+            }
+            else if (kilometer <= 4)
+            {
+                if (choice == 'A')
+                {
+                    fare *= 0.8;
+                    return fare;
+                }
+                return fare;
+            }
+            else if (kilometer >= 5)
+            {                
+                if (choice == 'A')
+                {
+                    kilometer -= 4;
+                    kilometer += fare;
+                    kilometer *= 0.8;
+                    return kilometer;
+                }
+                kilometer -= 4;
+                kilometer += fare;
+                return kilometer;
+            }
+            return 0;
+        }
+    }
+}
---
+++ CustoPassagemOnibus/Program.cs
@@ -0,0 +1,23 @@
+﻿using CustoPassagemOnibus.Entities;
+using System;
+
+namespace CustoPassagemOnibus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter kilometer to travel: ");
+            double kilometer = double.Parse(Console.ReadLine());
+            Console.WriteLine("Choose your status:");
+            Console.WriteLine("A. Student / Senior / PWD (20%)");
+            Console.WriteLine("B. Regular");
+            Console.Write("Enter choice: ");
+            char choice = char.Parse(Console.ReadLine());
+
+            CalcularCusto calcularCusto = new CalcularCusto(kilometer, choice);
+            Console.WriteLine("Hello, your fare is: " + calcularCusto.Calcular(kilometer, choice));
+            Main(args);
+        }
+    }
+}
